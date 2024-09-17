80c93edb9742d2c46a7c41373e0bd41ef5da8f4b
ZayanZaheer <m.zayan.zaheer@gmail.com>
2024-09-17 15:27:17 +0800

    Corrected a mistake to ensure proper database connection.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const {data: session} = useSession();
   const [providers, setProviders] = useState(null);
   const [toggleDropDown,setToggleDropDown] = useState(false)
   useEffect(() => {
@@ -24,12 +24,12 @@ const Nav = () => {
       </Link>
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn?(<div className='flex gap-3 md:gap-5'><Link href="/create-prompt" className='black_btn'>
-        Create Post</Link><button type='button' onClick={signOut} className='outline_btn'>Sign Out</button><Link href="/profile"><Image src="/assets/images/logo.svg" width={37} height={37} className='rounded-full' alt='Profile Picture'/></Link></div>):(<>{providers && Object.values(providers).map((provider) => (<button type='button' key={provider.name} onClick={() => signIn(provider.id)} className='black_btn'>Sign In</button>))}</>)}
+        {session?.user ?(<div className='flex gap-3 md:gap-5'><Link href="/create-prompt" className='black_btn'>
+        Create Post</Link><button type='button' onClick={signOut} className='outline_btn'>Sign Out</button><Link href="/profile"><Image src={session?.user.image} width={37} height={37} className='rounded-full' alt='Profile Picture'/></Link></div>):(<>{providers && Object.values(providers).map((provider) => (<button type='button' key={provider.name} onClick={() => signIn(provider.id)} className='black_btn'>Sign In</button>))}</>)}
       </div>
       {/*Mobile Navigation*/}
       <div className='sm:hidden flex relative'>
-          {isUserLoggedIn?(<div className='flex'><Image src="/assets/images/logo.svg" width={37} height={37} className='rounded-full' alt='Profile Picture' onClick={() => setToggleDropDown((prev) => !prev)}/>
+          {session?.user ?(<div className='flex'><Image src={session?.user.image} width={37} height={37} className='rounded-full' alt='Profile Picture' onClick={() => setToggleDropDown((prev) => !prev)}/>
           {toggleDropDown && (<div className='dropdown'><Link href='/profile' className='dropdown_link' onClick={() => setToggleDropDown(false)}>My Profile
           </Link>
           <Link href='/create-prompt' className='dropdown_link' onClick={() => setToggleDropDown(false)}>Create Prompt
